6253190396ae48e96a672e3baec7966282c86850
Ezequiel <ezepuglisi@gmail.com>
2022-09-07 12:10:24 -0300

    demo 3

--- server.js
+++ server.js
@@ -23,17 +23,20 @@
 
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fileRouter = require('./files.routes');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World simulacr')
+    res.send('Hello World simulacr, actualizado')
 })
 
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: './upload'
 }));
+
+app.use(fileRouter);
 
 app.listen(3000);
 console.log('Server on port 3000');
