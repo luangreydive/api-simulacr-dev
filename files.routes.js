288cec76a394f78cb958da59c2070f124e50b365
Ezequiel <ezepuglisi@gmail.com>
2022-10-18 15:55:24 -0300

    files deleted

--- files.routes.js
+++ files.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const {uploadFile} = require('./s3')
 const router = Router()
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     res.send('Hello World')
@@ -10,7 +11,15 @@ router.post('/upload/', async (req, res) => {
     //console.log(req.files['video'])
     const response = await uploadFile(req.files['video'])
     console.log(response)
+    console.log(req.files['video'])
     res.send('Archivo subido')
+    //unlink file
+
+    fs.unlink(req.files['video'].tempFilePath, (err) => {
+        if (err) throw err;
+        console.log('successfully deleted');
+    });
+
 })
 
 module.exports = router
