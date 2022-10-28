9b752efce637dc5b542be7c2ca902fa6ad61a29a
Ezequiel <ezepuglisi@gmail.com>
2022-10-28 16:20:11 -0300

    file name updated

--- s3.js
+++ s3.js
@@ -8,8 +8,6 @@ const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
 const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
 const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION
 
-
-
 const client = new S3Client({
   region: AWS_BUCKET_REGION,
   credentials: {
@@ -22,19 +20,19 @@ async function uploadFile(file) {
 
   const stream = fs.createReadStream(file.tempFilePath)
   const cliente = file.name.slice(0, file.name.indexOf('+'))
-  const id = file.name.slice(file.name.indexOf('+') + 1, file.name.indexOf('.'))
+  const id = file.name.slice(file.name.indexOf('_') + 1, file.name.indexOf('.'))
   const uploadParams = {
     Bucket: AWS_BUCKET_NAME,
     Key: file.name,
     Body: stream,
-    Metadata:{
-      'Cliente': cliente
+    Metadata: {
+      'Cliente': cliente,
     },
     Tagging: 'Cliente=' + cliente,
-    Tagging: 'id=' + id  
+    Tagging: 'id=' + id,
+    ContentType: 'video/mp4'
   }
 
-  
 
   const command = new PutObjectCommand(uploadParams)
 
